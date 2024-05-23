80bcb9258536b2767b4d033a52c2c0ae48eebd70
hayapi0208 <yhayasaska0208@gmail.com>
2024-05-23 20:26:50 +0900

    アクティブハッシュの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
 
   validates :nickname, presence: true
