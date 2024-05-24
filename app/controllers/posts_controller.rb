b86392c0011ec2ae80c69bf8f70d29c974852b88
hayapi0208 <yhayasaska0208@gmail.com>
2024-05-24 14:43:29 +0900

    post投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,12 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
