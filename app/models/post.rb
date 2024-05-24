51e1d9bf37351bb13ddeb80c17c9220b2ece244b
hayapi0208 <yhayasaska0208@gmail.com>
2024-05-24 11:55:07 +0900

    postモデルにバリデーション設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :title, presence: true
+  validates :text, presence: true
+  validates :prefecture_id, presence: true
+
 end
