75bfad0b1ae3c9204770c5912d94e8efbda84cf9
hayapi0208 <yhayasaska0208@gmail.com>
2024-05-24 19:59:09 +0900

    キーワード検索機能を実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,17 @@ class Post < ApplicationRecord
   validates :text, presence: true
   validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
 
+  def self.search(search)
+    if search.present?
+      prefecture_id = Prefecture.find_by(name: search)&.id
+      if prefecture_id.present?
+        Post.where(prefecture_id: prefecture_id)
+      else
+        Post.where('title LIKE ? OR text LIKE ?', "%#{search}%", "%#{search}%").order('created_at DESC')
+      end
+    else
+      Post.order('created_at DESC')
+    end
+  end
+
 end
