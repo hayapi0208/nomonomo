93b5378062a1488e97e1725197b0af7437c26fcf
hayapi0208 <yhayasaska0208@gmail.com>
2024-05-27 20:05:02 +0900

    mapsコントローラー・ルート・ビューの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
   get 'posts/index'
 
   root to: "posts#index"
+  
   resources :posts do
     collection do
       get 'search'
     end
   end
+
+  resources :maps
+
 end
