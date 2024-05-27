5f357c4752e843aef714f7d93119d3051c65cebb
hayapi0208 <yhayasaska0208@gmail.com>
2024-05-27 19:45:29 +0900

    GoogleMapsAPIの導入（キーは環境変数で設定）

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# dotenv-rails を読み込む
+Dotenv::Railtie.load
+
 module Nomonomo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
