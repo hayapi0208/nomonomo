7498389aa91491cabbb92dd03f02b162ef95c76f
hayapi0208 <yhayasaska0208@gmail.com>
2024-06-01 18:53:44 +0900

    マウスオーバーしたリンクが発光するように変更

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -1,5 +1,9 @@
-function post (){
-  console.log("OK")
-  
-};
-window.addEventListener("turbo:load", post);
+import $ from "jquery";
+
+$(function(){
+  $(document).on("mouseover", "a", function () {
+    $(this).css({ "box-shadow": "0 0 40px 10px #FFF,0 0 50px 10px #FFF", "background-color": "rgb(235, 236, 228)" });
+  }).on("mouseout", "a", function () {
+    $(this).css({ "box-shadow": "", "background-color": "" });
+  })
+});
