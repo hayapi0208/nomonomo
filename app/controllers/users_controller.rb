ded079d49c968b56192413ae9e7600cef53be9d4
hayapi0208 <yhayasaska0208@gmail.com>
2024-06-04 19:14:14 +0900

    コメント投稿者名からマイページへの遷移を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @nickname = current_user.nickname
-    @posts = current_user.posts
+    @user = User.find(params[:id])
+    @posts = @user.posts
   end
 end
